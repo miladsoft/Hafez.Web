36c91126e7cbd73669b01a753489d54a355367dc
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-19 18:28:01 +0330

    Latest updates

---
+++ Hafez.Web/wwwroot/sworker.js
@@ -0,0 +1,33 @@
+﻿self.addEventListener('install', function (event) {
+    console.log('Install', event);
+    event.waitUntil(
+        caches.open('v3').then(function (cache) {
+            return cache.addAll([
+                '/hafez/lib/twitter-bootstrap/css/bootstrap.min.css',
+                '/hafez/lib/jquery/jquery.min.js',
+                '/hafez/lib/twitter-bootstrap/js/bootstrap.min.js',
+                '/hafez/lib/vue/vue.min.js',
+                '/hafez/hafez.json',
+                '/hafez/lib/vue-animate/vue2-animate.css',
+                '/hafez/button.png',
+                '/hafez/button-noshadow.png',
+                '/'
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Fetching something ....', event);
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+});
